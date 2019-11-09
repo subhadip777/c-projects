f4f98ed55a88728df983bb74bccc533cf0abbce2
@subhadip777 <subhadipdey411@gmail.com>
2019-11-09 15:10:31 +0530

    2nd feature

---
+++ main IQ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int y;
+   float i,x;
+   for(y=1;y<=6;y++)
+   {
+       for(x=5.5;x<=12.5;x=x+.5)
+       {
+           i=2+(y+.5*x);
+           printf("y=%d,x=%f,i=%f\n",y,x,i);
+       }
+   }
+
+    return 0;
+}
